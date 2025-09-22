70a53f928c103abbf215435b3a31ff1ce7969aea
Debin <luodeb@outlook.com>
2025-09-22 10:36:45 +0800

    del target

--- target/debug/deps/rknpu2-9b7abe1b40a8c182.d
+++
@@ -1,5 +0,0 @@
-/home/orangepi/rknpu2-rslab/target/debug/deps/rknpu2-9b7abe1b40a8c182.d: rknpu2/src/main.rs
-
-/home/orangepi/rknpu2-rslab/target/debug/deps/librknpu2-9b7abe1b40a8c182.rmeta: rknpu2/src/main.rs
-
-rknpu2/src/main.rs:
--- target/debug/deps/rknpu2-fc373dfc2aa59918.d
+++
@@ -1,5 +0,0 @@
-/home/orangepi/rknpu2-rslab/target/debug/deps/rknpu2-fc373dfc2aa59918.d: rknpu2/src/main.rs
-
-/home/orangepi/rknpu2-rslab/target/debug/deps/librknpu2-fc373dfc2aa59918.rmeta: rknpu2/src/main.rs
-
-rknpu2/src/main.rs:
